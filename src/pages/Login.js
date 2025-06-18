a3ee3737e754cfcd726fee4d365bdc677064f5ee
MUHAMMAD MAHBUB SARWAR SHAFI <2054901049@student.bup.edu.bd>
2025-06-18 16:40:42 +0600

    image bug fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
             setError(err.message);
         }
         const token = await auth.currentUser.getIdToken();
-        const response = await fetch('https://professor-input-server.vercel.app/api/auth/login', {
+        const response = await fetch('http://localhost:5000/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ idToken: token }),
--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://professor-input-server.vercel.app/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
